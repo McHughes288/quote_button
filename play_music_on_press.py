96f6c73073c5244643cafc92ba18e1c7e2df7ac2
John Hughes <jpl.hughes@btinternet.com>
2020-05-13 13:23:43 +0100

    multi processing for led tasks - waiting and flashing to sound

--- play_music_on_press.py
+++ play_music_on_press.py
@@ -2,36 +2,54 @@ from raspberry.pi import RaspberryPi
 import numpy as np
 import random
 import time
+from multiprocessing import Process
 
 pi = RaspberryPi()
-GPIO = pi.setup_gpio()
+pi.setup_gpio()
 pi.update_available_sounds()
 
-while True:
-    for button_name, button_pin in pi.button_name_to_pin.items():
-        # Detect button press
-        if GPIO.input(button_pin) == GPIO.LOW:
-            print(f"Button {button_name} (pin {button_pin}) was pushed!")
+flash_process = None
+wait_process = None
 
-            # LEDS on
-            for pin in pi.led_pins:
-                GPIO.output(pin, GPIO.HIGH)
-            
-            # update sounds available in gdrive and get the relevant ones
-            pi.update_available_sounds()
-            available_files = pi.button_name_to_files[button_name]
-            
-            # play a random sound bite
-            if available_files:
-                sound_file_path = random.choice(available_files)
+try:
+    while True:
+        # LED turn on in waves for "waiting" state
+        if wait_process is None or not wait_process.is_alive():
+            if flash_process is None or not flash_process.is_alive():
+                wait_process = Process(target=pi.wave_leds, args=(0.1,))
+                wait_process.start()
+
+        for button_name in pi.button_names:
+            # Detect button press
+            if pi.button_pressed(button_name):
+                print(f"Button {button_name} was pushed!")
+                wait_process.terminate()
+
+                # Terminate flashing led process if running
+                if flash_process is not None and flash_process.is_alive():
+                    print("Terminating flash led process...")
+                    flash_process.terminate()
+                    pi.turn_leds_off()
+
+                # update sounds available in gdrive and get the relevant ones
+                pi.update_available_sounds()
+                available_files = pi.button_name_to_files[button_name]
+                
+                # pick a random sound bite
+                if available_files:
+                    sound_file_path = random.choice(available_files)  
+                else:
+                    sound_file_path = "/home/pi/mnt/gdrive/Brian/17.wav"
                 print(sound_file_path)
+                
+                # Flash LEDs as background process and play sound
+                flash_process = Process(target=pi.flash_to_sound, args=(sound_file_path,))
+                flash_process.start()
                 pi.play_sound(sound_file_path)
-            else:
-                pi.play_sound("/home/pi/mnt/gdrive/Brian/17.wav")
 
-            while GPIO.input(button_pin) == GPIO.LOW:
-                time.sleep(0.1)
-
-            # LEDs off
-            for pin in pi.led_pins:
-                GPIO.output(pin, GPIO.LOW)
+                # if button still held, just wait
+                while pi.button_pressed(button_name):
+                    time.sleep(0.05)
+except KeyboardInterrupt:
+    pass
+pi.GPIO.cleanup()
--- raspberry/pi.py
+++ raspberry/pi.py
@@ -10,13 +10,19 @@ import pygame
 import RPi.GPIO as GPIO
 import os
 from raspberry.util import get_sample_rate
-
+from scipy.io.wavfile import read
+import numpy as np
+import time
 
 class RaspberryPi:
     def __init__(self, microphone=False, speakers=True, camera=True):
         self.microphone = microphone
         self.speakers = speakers
         self.camera = camera
+
+        self.GPIO = GPIO
+
+        self.button_names = ["Brian", "Mowbros", "Random", "Alert"]
 
         self.button_name_to_pin = {
             "Brian": 38, 
@@ -35,17 +41,15 @@ class RaspberryPi:
         self.led_pins = [7, 11, 12, 16, 29, 31]
     
     def setup_gpio(self):
-        GPIO.setmode(GPIO.BOARD)
+        self.GPIO.setmode(self.GPIO.BOARD)
         
         # button pins
         for pin in self.button_name_to_pin.values():
-            GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            self.GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         
         # LED pins
         for pin in self.led_pins:
-            GPIO.setup(pin, GPIO.OUT)
-
-        return GPIO
+            self.GPIO.setup(pin, GPIO.OUT)
         
     def update_available_sounds(self):
         for name in self.button_name_to_files.keys():
@@ -65,6 +69,53 @@ class RaspberryPi:
         if wait_to_finish:
             while pygame.mixer.music.get_busy():
                 pygame.time.Clock().tick(10)
+    
+    def button_pressed(self, button_name):
+        button_pin = self.button_name_to_pin[button_name]
+        return self.GPIO.input(button_pin) == self.GPIO.LOW
+
+    def flash_to_sound(self, sound_file_path, update_every=0.01):
+        sound = read(sound_file_path)
+        sampling_rate = get_sample_rate(sound_file_path)
+
+        sound = np.array(sound[1], dtype=float)
+        mono = sound[:,0] + sound[:,1] / 2.0
+        loudness = abs(mono) / abs(mono).max()
+
+        # downsample by max pooling over update_every chunks
+        remainder = int(loudness.shape[0] % (sampling_rate * update_every))
+        loudness = loudness[0:-remainder]
+        loudness = loudness.reshape(-1, int(sampling_rate * update_every))
+        loudness = np.max(loudness, 1)
+
+        # LEDs on if over a volume threshold
+        for level in loudness:
+            if level > 0.6:
+                self.turn_leds_on()
+            else:
+                self.turn_leds_off()
+            time.sleep(update_every)
+        
+        self.turn_leds_off()
+
+    def turn_leds_on(self):
+        for pin in self.led_pins:
+            self.GPIO.output(pin, self.GPIO.HIGH)
+
+    def turn_leds_off(self):
+        for pin in self.led_pins:
+            self.GPIO.output(pin, self.GPIO.LOW)
+
+    def wave_leds(self, length):
+        time.sleep(1)
+        while 1:
+            for pin in self.led_pins:
+                self.GPIO.output(pin, self.GPIO.HIGH)
+                time.sleep(length)
+            for pin in self.led_pins:
+                self.GPIO.output(pin, self.GPIO.LOW)
+                time.sleep(length)
+
 
 
     # def play_recording(self, wav_file, save_png=False):
